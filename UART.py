fae24dedde06d412308a3f6b362e53f6a174702f
zacshen041 <52594842+zacshen041@users.noreply.github.com>
2021-07-27 11:56:30 +0800

    update

--- UART.py
+++ UART.py
@@ -11,6 +11,7 @@ cam0 = CSICamera(width=1080, height=720, capture_width=1080, capture_height=720,
 
 def STM(ID, reg, data):
     print("#APB"+ID+reg+data)
+    fc_com1.write(b"change\n")
 
 fc_com1 = serial.Serial(
     port="/dev/ttyTHS0",
@@ -20,18 +21,38 @@ fc_com1 = serial.Serial(
     stopbits=serial.STOPBITS_ONE,
 )
 time.sleep(1)
+nowTime=time.time()
+if(time.time()-nowTime>1):
+        #print('waiting')
+        nowTime=time.time()
+data='wait'
 ID='255'
 reg='0xqCF'
 nowTime=time.time()
-while(True):
-    
-    if(time.time()-nowTime>1):
-        #print('waiting')
-        nowTime=time.time()
-    if cv2.waitKey(1)==ord('i'):
-        data=input("Please enter a string:\n")
-        STM(ID,reg,data)
-    if cv2.waitKey(1)==ord('q'):
-        break
+clockTime=time.time()
 
+while(True):
+    while(data!='start'):#wait until start
+        data=input("Please enter a string:\n")
+        if(data=='start'):
+            STM(ID,reg,data)
+            nowTime=time.time()
+
+    if fc_com1.inWaiting() > 0 :#receiver
+        messege = fc_com1.readline() 
+        print(messege)
+
+    if(time.time()-clockTime>1):#transmitter
+        fc_com1.write(b"change\n")
+        clockTime=time.time()
+
+    if cv2.waitKey(1)==ord('q'):#q or 10sec end the process
+        break
+    if(time.time()-nowTime>10):
+        break
+        nowTime=time.time()
+
+    image0 = cam0.read()
+    cv2.imshow("Dialation Image",image0)
 fc_com1.close()
+cv2.destoryAllWindows()
